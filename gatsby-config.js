e50b6548605714a157e14cc71a5474b4b4745301
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2020-01-29 13:29:21 +0530

    Add google analytics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-8709243-5`,
       },
     },
     `gatsby-plugin-feed`,
