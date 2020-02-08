e04c8e1ef10e06b91dc9022e6ba6f5aed6ff0bf3
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2020-02-08 11:46:43 +0530

    Add google adsense plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-8692104840351793`
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
